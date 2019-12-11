4e8887ca2ecf3d7b4426b03196ebdfbf89e1b040
TE-Good <tom.good1990@gmail.com>
2019-12-11 15:43:24 +0000

    nav bar functionality v1

--- app.js
+++ app.js
@@ -1,5 +1,23 @@
 window.addEventListener('DOMContentLoaded', () => {
   console.log('hello world')
 
+  // const projectTitle = document.querySelector('.project-header')
+  const projectNav = document.querySelector('.project-titles')
+  console.log(projectNav)
+
+  window.addEventListener('scroll', () => {
+    console.log(window.scrollY)
+    // if (window.scrollY >= 753) {
+    if (window.scrollY >= 829) {
+      console.log('fix')
+      // projectTitle.classList.add('fixed-title')
+      projectNav.classList.add('fixed-nav')
+    } else {
+      console.log('unfix')
+      // projectTitle.classList.remove('fixed-title')
+      projectNav.classList.remove('fixed-nav')
+    }
+  })
+
 
 }) // END OF SCRIPT
